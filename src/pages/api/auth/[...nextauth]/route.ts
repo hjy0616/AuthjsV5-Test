0ed6eaf0ae9662c52b8aef4634af667073add2aa
hjy0616 <ghkdwjddus52@gmail.com>
2024-01-09 12:13:17 +0900

    first commit

---
+++ src/pages/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+import NaverProvider from "next-auth/providers/naver";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "../../../../../prisma/client";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID!,
+      clientSecret: process.env.NAVER_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password!
+        );
+        return passwordMatch ? user : null;
+      },
+    }),
+  ],
+});
+
+export { handler as Get, handler as POST };
